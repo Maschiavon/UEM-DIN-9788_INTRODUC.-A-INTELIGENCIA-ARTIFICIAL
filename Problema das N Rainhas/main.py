752a13b9fc9bb2931937398a93791e4b8520273f
Matheus Augusto Schiavon <45128871+Maschiavon@users.noreply.github.com>
2023-05-01 12:05:29 -0300

    Commit inicial

---
+++ Problema das N Rainhas/main.py
@@ -0,0 +1,142 @@
+import random
+import time
+
+def random_chromosome(size):  # making random chromosomes
+    return [random.randint(1, nq) for _ in range(nq)]
+
+def fitness(chromosome):
+    horizontal_collisions = sum([chromosome.count(queen) - 1 for queen in chromosome]) / 2
+    diagonal_collisions = 0
+
+    n = len(chromosome)
+    left_diagonal = [0] * 2 * n
+    right_diagonal = [0] * 2 * n
+    for i in range(n):
+        left_diagonal[i + chromosome[i] - 1] += 1
+        right_diagonal[len(chromosome) - i + chromosome[i] - 2] += 1
+
+    diagonal_collisions = 0
+    for i in range(2 * n - 1):
+        counter = 0
+        if left_diagonal[i] > 1:
+            counter += left_diagonal[i] - 1
+        if right_diagonal[i] > 1:
+            counter += right_diagonal[i] - 1
+        diagonal_collisions += counter / (n - abs(i - n + 1))
+
+    return int(maxFitness - (horizontal_collisions + diagonal_collisions))  # 28-(2+3)=23
+
+def probability(chromosome, fitness):
+    return fitness(chromosome) / maxFitness
+
+def random_pick(population, probabilities):
+    populationWithProbabilty = zip(population, probabilities)
+    total = sum(w for c, w in populationWithProbabilty)
+    r = random.uniform(0, total)
+    upto = 0
+    for c, w in zip(population, probabilities):
+        if upto + w >= r:
+            return c
+        upto += w
+    assert False, "Shouldn't get here"
+
+def reproduce(x, y):  # doing cross_over between two chromosomes
+    n = len(x)
+    c = random.randint(0, n - 1)
+    return x[0:c] + y[c:n]
+
+def mutate(x):  # randomly changing the value of a random index of a chromosome
+    n = len(x)
+    c = random.randint(0, n - 1)
+    m = random.randint(1, n)
+    x[c] = m
+    return x
+
+def genetic_queen(population, fitness, i):
+    mutation_probability = (0.03 if i > 9 else 0.1)
+    new_population = []
+    probabilities = [probability(n, fitness) for n in population]
+    for i in range(len(population)):
+        x = random_pick(population, probabilities)  # best chromosome 1
+        y = random_pick(population, probabilities)  # best chromosome 2
+        child = reproduce(x, y)  # creating two new chromosomes from the best 2 chromosomes
+        if random.random() < mutation_probability:
+            child = mutate(child)
+        #print_chromosome(child)
+        new_population.append(child)
+        if fitness(child) == maxFitness: break
+    return new_population
+
+def print_chromosome(chrom):
+    print("Chromosome = {},  Fitness = {}"
+          .format(str(chrom), fitness(chrom)))
+
+if __name__ == "__main__":
+    nq = 8#int(input("Enter Number of Queens: "))  # say N = 8
+    # Normal (1 ~ 3)
+    # Número da população Normal (4 ~ 6)
+    # Critério de parada número de gerações (7 ~ 9)
+    # Porcentagem de mutação (10 ~ 12)
+    n_populacao = 100
+    tempo_medio = 0
+    cr_max = 0
+    for i in range(1, 13):
+        print("Solução Numero {}".format(i))
+        start = time.time()
+        maxFitness = (nq * (nq - 1)) / 2  # 8*7/2 = 28
+
+        if i > 3 and i < 7:
+            n_populacao = 200
+        population = [random_chromosome(nq) for _ in range(n_populacao)]
+
+        generation = 1
+
+        while not maxFitness in [fitness(chrom) for chrom in population]:
+            #print("=== Generation {} ===".format(generation))
+            population = genetic_queen(population, fitness, i)
+            #print("")
+            #print("Maximum Fitness = {}".format(max([fitness(n) for n in population])))
+            if i > 6 and i < 10 and generation >= 1000:
+                maximo = 0
+                for chrom in population:
+                    num = fitness(chrom)
+                    if maximo < num:
+                        maximo = num
+                        cr_max = chrom.copy()
+                maxFitness = maximo
+                break
+            generation += 1
+        end = time.time()
+        chrom_out = []
+        tempo = end-start
+        tempo_medio += tempo
+        print("Tempo Execução: {}!".format(tempo))
+        print("Solved in Generation {}!".format(generation - 1))
+        print("Fitness {}!".format(maxFitness))
+        for chrom in population:
+            if fitness(chrom) == maxFitness:
+                print("");
+                print("One of the solutions: ")
+                chrom_out = chrom
+                print_chromosome(chrom)
+
+        chrom_out = (cr_max if not chrom_out else chrom_out)
+        board = []
+        print(chrom_out)
+        for x in range(nq):
+            board.append(["x"] * nq)
+
+        for j in range(nq):
+            board[nq - chrom_out[j]][j] = "Q"
+
+        def print_board(board):
+            for row in board:
+                print(" ".join(row))
+
+        print()
+        print_board(board)
+        print()
+        if i % 3 == 0:
+            tempo_medio /= 3
+            print("Tempo Médio: {}!".format(tempo_medio))
+            tempo_medio = 0
